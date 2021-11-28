8515af0143b15e0fb5526a83fc29a61de8c4b88c
Anug Saha <anugsaha@gmail.com>
2021-11-28 18:19:54 -0500

    impement header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
-import HomePage from "./pages/homepage"
+import HomePage from "./pages/homepage";
 import ShopPage from "./pages/shop/shop";
+import Header from "./components/header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
