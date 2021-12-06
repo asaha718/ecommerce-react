b36c5d763ce64547c211010ae3f18b5e1f7cc4f0
Anug Saha <anugsaha@gmail.com>
2021-12-06 16:58:34 -0500

    add selector to cart dropdown to save rendering

--- src/components/cart-dropdown.jsx
+++ src/components/cart-dropdown.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import CustomButton from "./custom-button";
 import CartItem from "./cart-item";
+import { selectCartItems } from "../redux/cart/cart.selectors";
 
 import "../styles/cart-dropdown.scss";
 
@@ -16,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
