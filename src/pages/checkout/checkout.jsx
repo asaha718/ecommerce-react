952a99e47e77d8c5cf198288870c769d723ff4f7
Anug Saha <anugsaha@gmail.com>
2021-12-06 22:52:12 -0500

    route to checkout page

---
+++ src/pages/checkout/checkout.jsx
@@ -0,0 +1,9 @@
+import React from "react"; 
+import "../../styles/checkout.scss";
+
+const CheckoutPage=()=> { 
+    return( 
+        <div>Checkout Page</div>
+    )
+} 
+export default CheckoutPage
