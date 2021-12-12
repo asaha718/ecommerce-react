2a73aa94cc23985974e40295ddf601519f02b741
Anug Saha <anugsaha@gmail.com>
2021-12-12 01:29:33 -0500

    hook up remove item action to reducer and util function

--- src/components/checkout-item.jsx
+++ src/components/checkout-item.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { clearItemFromCart, addItem } from "../redux/cart/cart.actions";
+import { clearItemFromCart, addItem, removeItem } from "../redux/cart/cart.actions";
 import "../styles/checkout-item.scss";
 
 const CheckoutItem = ({ cartItem, clearItem }) => {
@@ -11,7 +11,15 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
@@ -23,6 +31,7 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 const mDTP = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
   addItem: (item) => dispatch(addItem(item)),
+  removeItem: item=> dispatch(removeItem(item)), 
 });
 
 export default connect(null, mDTP)(CheckoutItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,9 +1,14 @@
-import { TOGGLE_CART_HIDDEN, ADD_ITEM, CLEAR_ITEM_FROM_CART} from "./cart.actions";
-import { addItemToCart } from "./cart.utils";
+import {
+  TOGGLE_CART_HIDDEN,
+  ADD_ITEM,
+  REMOVE_ITEM,
+  CLEAR_ITEM_FROM_CART,
+} from "./cart.actions";
+import { addItemToCart, removeItemFromCart} from "./cart.utils";
 
 const INITIAL_STATE = {
-  hidden: true, 
-  cartItems: [], 
+  hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -17,6 +22,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
       };
     case CLEAR_ITEM_FROM_CART:
       return {
