74b46a7ce842d28f1c91cc4a6d12b802e1e11c7c
Anug Saha <anugsaha@gmail.com>
2021-12-14 21:23:54 -0500

    move directory over to reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
 export default combineReducers({ 
-    cart: cartReducer
+    cart: cartReducer, 
+    directory: directoryReducer, 
 }); 
