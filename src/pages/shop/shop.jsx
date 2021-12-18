11b1cc37104f59da17d24feac54639c8d9ed41e9
Anug Saha <anugsaha@gmail.com>
2021-12-18 14:37:49 -0500

    refactor shop data to put into redux store

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollections } from "../../redux/shop/shop.selector";
 import PreviewCollection from "../../components/preview-collection";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { collections: SHOP_DATA };
-  }
+const ShopPage = ({collections}) => {
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <PreviewCollection key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <PreviewCollection key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
 
-export default ShopPage; 
+const mSTP=(state)=>createStructuredSelector({ 
+collections: selectCollections
+})
+export default connect(mSTP)(ShopPage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
 import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer"; 
 
 export default combineReducers({ 
     cart: cartReducer, 
     directory: directoryReducer, 
+    shop: shopReducer, 
 }); 
