4059019ada270391b5e2da0f35485eedf1e00c67
Anug Saha <anugsaha@gmail.com>
2021-12-19 16:34:32 -0500

    modify imports for object refactoring

--- src/components/collections-overview.jsx
+++ src/components/collections-overview.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../redux/shop/shop.selector";
+import { selectCollectionsForPreview } from "../redux/shop/shop.selector";
 import PreviewCollection from "./preview-collection";
 import "../styles/collections-overview.scss";
 
@@ -17,7 +17,7 @@ const CollectionsOverview = ({ collections }) => {
 
 const mSTP = (state) =>
   createStructuredSelector({
-    collections: selectCollections,
+    collections: selectCollectionsForPreview,
 });
 
 export default connect(mSTP)(CollectionsOverview);
